197bdd845d8e96a5ec2bf69640a2418569825c1a
Coderfrog256 <johnnie.bluntnose@gmail.com>
2024-12-15 20:58:42 -0800

    Add my software synths.

--- picker.ts
+++ picker.ts
@@ -1,6 +1,6 @@
 interface Synth {
     name: string;
-    type: "Hardware" | "Software";
+    style: "Hardware" | "Software";
     roles: string[];
     numberChannels: number;
 }
@@ -16,173 +16,602 @@ const roles = [
 const synthList: Synth[] = [
     {
         name: "Hydrasynth",
-        type: "Hardware",
+        style: "Hardware",
         roles: ["Lead", "Bass", "Pad"],
         numberChannels: 1
     },
     {
         name: "Opsix",
-        type: "Hardware",
+        style: "Hardware",
         roles: ["Lead", "Bass", "Pad"],
         numberChannels: 1
     },
     {
         name: "Virus TI",
-        type: "Hardware",
+        style: "Hardware",
         roles: ["Lead", "Bass", "Pad"],
         numberChannels: 16
     },
     {
         name: "Iridium",
-        type: "Hardware",
+        style: "Hardware",
         roles: ["Lead", "Bass", "Pad"],
         numberChannels: 2
     },
     {
         name: "Digitone",
-        type: "Hardware",
+        style: "Hardware",
         roles: ["Sequencer", "Lead", "Bass", "Pad", "Drum"],
         numberChannels: 16
     },
     {
         name: "M8",
-        type: "Hardware",
+        style: "Hardware",
         roles: ["Sequencer", "Lead", "Bass", "Pad", "Drum"],
         numberChannels: 8
     },
     {
         name: "Modwave",
-        type: "Hardware",
+        style: "Hardware",
         roles: ["Lead", "Bass", "Pad"], // Technically has drums, but nah.
         numberChannels: 2
     },
     {
         name: "Matriarch",
-        type: "Hardware",
+        style: "Hardware",
         roles: ["Lead", "Bass", "Pad"],
         numberChannels: 1
     },
     {
         name: "Taiga",
-        type: "Hardware",
+        style: "Hardware",
         roles: ["Lead", "Bass", "Pad"],
         numberChannels: 1
     },
     {
         name: "MPC",
-        type: "Hardware",
+        style: "Hardware",
         roles: ["Sequencer", "Lead", "Bass", "Pad", "Drum"],
         numberChannels: 16 // Infinite?
     },
     {
         name: "Summit",
-        type: "Hardware",
+        style: "Hardware",
         roles: ["Lead", "Bass", "Pad"],
         numberChannels: 2
     },
     {
         name: "MC707",
-        type: "Hardware",
+        style: "Hardware",
         roles: ["Lead", "Bass", "Pad", "Drum"], // Technically sequencer too, but not hooked up for midi out
         numberChannels: 8
     },
     {
         name: "Deluge",
-        type: "Hardware",
+        style: "Hardware",
         roles: ["Sequencer", "Lead", "Bass", "Pad", "Drum"],
         numberChannels: 16
     },
     {
         name: "Ultra Proteus",
-        type: "Hardware",
+        style: "Hardware",
         roles: ["Lead", "Bass", "Pad", "Drum"],
         numberChannels: 16 // Technically 32, but only 16 midi channels connected
     },
     {
         name: "Proteus 2000",
-        type: "Hardware",
+        style: "Hardware",
         roles: ["Lead", "Bass", "Pad", "Drum"],
         numberChannels: 16 // Technically 32, but only 16 midi channels connected
     },
     {
         name: "SC880",
-        type: "Hardware",
+        style: "Hardware",
         roles: ["Lead", "Bass", "Pad", "Drum"],
         numberChannels: 16 // Technically 32, but only 16 midi channels connected
     },
     {
         name: "JV1080",
-        type: "Hardware",
+        style: "Hardware",
         roles: ["Lead", "Bass", "Pad", "Drum"],
         numberChannels: 16
     },
     {
         name: "E6400 Ultra",
-        type: "Hardware",
+        style: "Hardware",
         roles: ["Lead", "Bass", "Pad", "Drum"],
         numberChannels: 16
     },
     {
         name: "WaveState",
-        type: "Hardware",
+        style: "Hardware",
         roles: ["Lead", "Bass", "Pad"], // Technically has drums, but nah.
         numberChannels: 4
     },
     {
         name: "XFM",
-        type: "Hardware",
+        style: "Hardware",
         roles: ["Lead", "Bass", "Pad"],
         numberChannels: 1
     },
     {
         name: "JD-08",
-        type: "Hardware",
+        style: "Hardware",
         roles: ["Lead", "Bass", "Pad"],
         numberChannels: 2
     },
     {
         name: "Microfreak",
-        type: "Hardware",
+        style: "Hardware",
         roles: ["Lead", "Bass", "Pad"],
         numberChannels: 1
     },
     {
         name: "Hapax",
-        type: "Hardware",
+        style: "Hardware",
         roles: ["Sequencer"],
         numberChannels: 1 // Just a sequencer
     },
     {
         name: "Torso T-1",
-        type: "Hardware",
+        style: "Hardware",
         roles: ["Sequencer"],
         numberChannels: 1 // Just a sequencer
     },
     {
         name: "MegaSynth",
-        type: "Hardware",
+        style: "Hardware",
         roles: ["Lead", "Bass", "Pad", "Drum"],
         numberChannels: 6
     },
     {
         name: "Modx",
-        type: "Hardware",
+        style: "Hardware",
         roles: ["Lead", "Bass", "Pad", "Drum"],
         numberChannels: 16
     },
     {
         name: "Osmose",
-        type: "Hardware",
+        style: "Hardware",
         roles: ["Lead", "Bass", "Pad"],
         numberChannels: 1
     },
     {
         name: "Take 5",
-        type: "Hardware",
+        style: "Hardware",
         roles: ["Lead", "Bass", "Pad"],
         numberChannels: 1
-    }
+    },
+    // Software (Note all are listed as 16 channels, as there's no real limit)
+    // Free
+    {
+        name: "Helm",
+        style: "Software",
+        roles: ["Lead", "Bass", "Pad"],
+        numberChannels: 16
+    },
+    {
+        name: "Vital",
+        style: "Software",
+        roles: ["Lead", "Bass", "Pad"],
+        numberChannels: 16
+    },
+    {
+        name: "Surge",
+        style: "Software",
+        roles: ["Lead", "Bass", "Pad"],
+        numberChannels: 16
+    },
+    // Misc Paid
+    {
+        name: "Omnisphere",
+        style: "Software",
+        roles: ["Lead", "Bass", "Pad", "Drum"], // Drum is a stretch, but it can do it.
+        numberChannels: 16
+    },
+    {
+        name: "Phase Plant",
+        style: "Software",
+        roles: ["Lead", "Bass", "Pad"],
+        numberChannels: 16
+    },
+    {
+        name: "Diva",
+        style: "Software",
+        roles: ["Lead", "Bass", "Pad"],
+        numberChannels: 16
+    },
+    {
+        name: "Hive",
+        style: "Software",
+        roles: ["Lead", "Bass", "Pad"],
+        numberChannels: 16
+    },
+    {
+        name: "Jura",
+        style: "Software",
+        roles: ["Lead", "Bass", "Pad"],
+        numberChannels: 16
+    },
+    {
+        name: "TAL Sampler",
+        style: "Software",
+        roles: ["Lead", "Bass", "Pad", "Drum"],
+        numberChannels: 16
+    },
+
+    // Korg
+    {
+        name: "Electribe-R VST",
+        style: "Software",
+        roles: ["Drum"],
+        numberChannels: 16
+    },
+    {
+        name: "M1 VST",
+        style: "Software",
+        roles: ["Lead", "Bass", "Pad", "Drum"],
+        numberChannels: 16
+    },
+    {
+        name: "Microkorg VST",
+        style: "Software",
+        roles: ["Lead", "Bass", "Pad"],
+        numberChannels: 16
+    },
+    {
+        name: "Minikorg 700S VST",
+        style: "Software",
+        roles: ["Lead", "Bass", "Pad"],
+        numberChannels: 16
+    },
+    {
+        name: "Monopoly VST",
+        style: "Software",
+        roles: ["Lead", "Bass", "Pad"],
+        numberChannels: 16
+    },
+    {
+        name: "Korg MS-20 VST",
+        style: "Software",
+        roles: ["Lead", "Bass", "Pad"], // Pads is weird, but it can do it.
+        numberChannels: 16
+    },
+    {
+        name: "OpSix VST",
+        style: "Software",
+        roles: ["Lead", "Bass", "Pad"],
+        numberChannels: 16
+    },
+    {
+        name: "Polysix VST",
+        style: "Software",
+        roles: ["Lead", "Bass", "Pad"],
+        numberChannels: 16
+    },
+    {
+        name: "Prophecy VST",
+        style: "Software",
+        roles: ["Lead", "Bass", "Pad"],
+        numberChannels: 16
+    },
+    {
+        name: "Triton VST",
+        style: "Software",
+        roles: ["Lead", "Bass", "Pad", "Drum"],
+        numberChannels: 16
+    },
+    {
+        name: "Triton Extreme VST",
+        style: "Software",
+        roles: ["Lead", "Bass", "Pad", "Drum"],
+        numberChannels: 16
+    },
+    {
+        name: "Wavestation VST",
+        style: "Software",
+        roles: ["Lead", "Bass", "Pad", "Drum"],
+        numberChannels: 16
+    },
+    {
+        name: "Korg WaveState VST",
+        style: "Software",
+        roles: ["Lead", "Bass", "Pad"], // Technically has drums, but nah.
+        numberChannels: 16
+    },
+
+    // Arturia
+    {
+        name: "Pigments",
+        style: "Software",
+        roles: ["Lead", "Bass", "Pad"],
+        numberChannels: 16
+    },
+    {
+        name: "Acid V",
+        style: "Software",
+        roles: ["Lead", "Bass"],
+        numberChannels: 16
+    },
+    {
+        name: "2600 V",
+        style: "Software",
+        roles: ["Lead", "Bass", "Pad"],
+        numberChannels: 16
+    },
+    {
+        name: "Augmented Strings",
+        style: "Software",
+        roles: ["Pad"], // Not really a lead or bass
+        numberChannels: 16
+    },
+    {
+        name: "Augmented Voices",
+        style: "Software",
+        roles: ["Pad"], // Not really a lead or bass?
+        numberChannels: 16
+    },
+    {
+        name: "B-3 V",
+        style: "Software",
+        roles: ["Lead", "Bass", "Pad"], // Does bass make sense?
+        numberChannels: 16
+    },
+    {
+        name: "Buchla Easel V",
+        style: "Software",
+        roles: ["Lead", "Bass", "Pad"], // Does pad make sense?
+        numberChannels: 16
+    },
+    {
+        name: "Clavinet V",
+        style: "Software",
+        roles: ["Lead", "Bass"], // Does bass make sense?
+        numberChannels: 16
+    },
+    {
+        name: "CMI V",
+        style: "Software",
+        roles: ["Lead", "Bass", "Pad", "Drum"],
+        numberChannels: 16
+    },
+    {
+        name: "CS-80 V",
+        style: "Software",
+        roles: ["Lead", "Bass", "Pad"],
+        numberChannels: 16
+    },
+    {
+        name: "CZ V",
+        style: "Software",
+        roles: ["Lead", "Bass", "Pad"],
+        numberChannels: 16
+    },
+    {
+        name: "DX7 V",
+        style: "Software",
+        roles: ["Lead", "Bass", "Pad"],
+        numberChannels: 16
+    },
+
+    {
+        name: "Emulator II V",
+        style: "Software",
+        roles: ["Lead", "Bass", "Pad", "Drum"],
+        numberChannels: 16
+    },
+    {
+        name: "Farfisa V",
+        style: "Software",
+        roles: ["Lead", "Bass", "Pad"],
+        numberChannels: 16
+    },
+    {
+        name: "Jun-6 V",
+        style: "Software",
+        roles: ["Lead", "Bass", "Pad"],
+        numberChannels: 16
+    },
+    {
+        name: "Jup-8 V",
+        style: "Software",
+        roles: ["Lead", "Bass", "Pad"],
+        numberChannels: 16
+    },
+    {
+        name: "Arturia MS-20 V",
+        style: "Software",
+        roles: ["Lead", "Bass", "Pad"], // Pads is weird, but it can do it.
+        numberChannels: 16
+    },
+    {
+        name: "Matrix-12 V",
+        style: "Software",
+        roles: ["Lead", "Bass", "Pad"],
+        numberChannels: 16
+    },
+    {
+        name: "Melotron V",
+        style: "Software",
+        roles: ["Lead", "Bass", "Pad"],
+        numberChannels: 16
+    },
+    {
+        name: "Mini V",
+        style: "Software",
+        roles: ["Lead", "Bass", "Pad"], // Does pad make sense?
+        numberChannels: 16
+    },
+    {
+        name: "Modular V",
+        style: "Software",
+        roles: ["Lead", "Bass", "Pad"],
+        numberChannels: 16
+    },
+    {
+        name: "OB-Xa V",
+        style: "Software",
+        roles: ["Lead", "Bass", "Pad"],
+        numberChannels: 16
+    },
+    {
+        name: "Piano V",
+        style: "Software",
+        roles: ["Lead", "Bass"], // Does "pad" make sense for a piano?
+        numberChannels: 16
+    },
+    {
+        name: "Prophet V",
+        style: "Software",
+        roles: ["Lead", "Bass", "Pad"],
+        numberChannels: 16
+    },
+    {
+        name: "Prophet VS V",
+        style: "Software",
+        roles: ["Lead", "Bass", "Pad"],
+        numberChannels: 16
+    },
+    {
+        name: "SEM V",
+        style: "Software",
+        roles: ["Lead", "Bass", "Pad"],
+        numberChannels: 16
+    },
+    {
+        name: "Solina V",
+        style: "Software",
+        roles: ["Lead", "Bass", "Pad"], // Does bass make sense?
+        numberChannels: 16
+    },
+    {
+        name: "SQ-80 V",
+        style: "Software",
+        roles: ["Lead", "Bass", "Pad"],
+        numberChannels: 16
+    },
+    {
+        name: "Stage-73 V",
+        style: "Software",
+        roles: ["Lead", "Bass"], // Does "pad" make sense for a piano?
+        numberChannels: 16
+    },
+    {
+        name: "Synclavier V",
+        style: "Software",
+        roles: ["Lead", "Bass", "Pad", "Drum"],
+        numberChannels: 16
+    },
+    {
+        name: "Synthi V",
+        style: "Software",
+        roles: ["Lead", "Bass", "Pad"],
+        numberChannels: 16
+    },
+    {
+        name: "Wurli V",
+        style: "Software",
+        roles: ["Lead", "Bass"], // Does "pad" make sense for a piano?
+        numberChannels: 16
+    },
+
+
+    // NI
+    {
+        name: "Battery",
+        style: "Software",
+        roles: ["Drum"],
+        numberChannels: 16
+    },
+    {
+        name: "FM8",
+        style: "Software",
+        roles: ["Lead", "Bass", "Pad"],
+        numberChannels: 16
+    },
+    {
+        name: "Kontakt",
+        style: "Software",
+        roles: ["Lead", "Bass", "Pad", "Drum"],
+        numberChannels: 16
+    },
+    {
+        name: "Massive",
+        style: "Software",
+        roles: ["Lead", "Bass", "Pad"],
+        numberChannels: 16
+    },
+    {
+        name: "Massive X",
+        style: "Software",
+        roles: ["Lead", "Bass", "Pad"],
+        numberChannels: 16
+    },
+    {
+        name: "Reaktor",
+        style: "Software",
+        roles: ["Lead", "Bass", "Pad"],
+        numberChannels: 16
+    },
+
+    // Bitwig
+    {
+        name: "Bitwig",
+        style: "Software",
+        roles: ["Sequencer"],
+        numberChannels: 16
+    },
+    {
+        name: "Bitwig PolyGrid",
+        style: "Software",
+        roles: ["Lead", "Bass", "Pad"],
+        numberChannels: 16
+    },
+    {
+        name: "Bitwig Drum Machine",
+        style: "Software",
+        roles: ["Drum"],
+        numberChannels: 16
+    },
+    {
+        name: "Bitwig FM-4",
+        style: "Software",
+        roles: ["Lead", "Bass", "Pad"],
+        numberChannels: 16
+    },
+    {
+        name: "Bitwig Organ",
+        style: "Software",
+        roles: ["Lead", "Bass", "Pad"], // Does bass make sense?
+        numberChannels: 16
+    },
+    {
+        name: "Bitwig Polysynth",
+        style: "Software",
+        roles: ["Lead", "Bass", "Pad"],
+        numberChannels: 16
+    },
+    {
+        name: "Bitwig Sampler",
+        style: "Software",
+        roles: ["Lead", "Bass", "Pad", "Drum"],
+        numberChannels: 16
+    },
+    {
+        name: "Bitwig Phase-4",
+        style: "Software",
+        roles: ["Lead", "Bass", "Pad"],
+        numberChannels: 16
+    },
+    {
+        name: "Bitwig Polymer",
+        style: "Software",
+        roles: ["Lead", "Bass", "Pad"],
+        numberChannels: 16
+    },
 ]
+
+let currentStyle = "Hybrid";
 
 class SynthRole extends HTMLDivElement {
     role: string;
@@ -204,6 +633,7 @@ class SynthRole extends HTMLDivElement {
         const nullOption = document.createElement("option");
         nullOption.textContent = "None";
         this._selection.appendChild(nullOption);
+        shuffle(synthList);
         for (const synth of synthList) {
             if (synth.roles.includes(role)) {
                 const option = document.createElement("option");
@@ -239,12 +669,26 @@ class SynthRole extends HTMLDivElement {
             return true;
         }
         const channelsUsed = rolePickers.filter(role => role.synth === synth && role !== this).length;
-        if (channelsUsed >= synth.numberChannels) {
+        if (channelsUsed >= synth.numberChannels || (synth.style !== currentStyle && currentStyle !== "Hybrid")) {
             return false;
         }
         this.synth = synth;
         this._selection.value = synth.name;
         return true;
+    }
+
+    setStyle(style: string) {
+        for (const option of this._selection.options) {
+            if (option.textContent === "None") {
+                continue;
+            }
+            const synth = synthList.find(synth => synth.name === option.textContent);
+            const hide = synth?.style !== style && style !== "Hybrid";
+            option.hidden = hide;
+            if (hide && this.synth?.name === option.textContent) {
+                this.tryAssignSynth(undefined);
+            }
+        }
     }
 }
 window.customElements.define("synth-role", SynthRole, { extends: "div" });
@@ -279,7 +723,28 @@ function randomizeAll() {
 }
 
 const root = document.getElementById("root");
-root.innerHTML = "";
+const stylePicker = document.createElement("div");
+stylePicker.classList.add("style-picker");
+for (const style of ["Hardware", "Hybrid", "Software"]) {
+    const radio = document.createElement("input");
+    radio.type = "radio";
+    radio.name = "style";
+    radio.value = style;
+    radio.id = style;
+    radio.addEventListener("change", () => {
+        currentStyle = style;
+        rolePickers.forEach(role => role.setStyle(style));
+    });
+    if (style === currentStyle) {
+        radio.checked = true;
+    }
+    const label = document.createElement("label");
+    label.htmlFor = style;
+    label.textContent = style;
+    stylePicker.appendChild(radio);
+    stylePicker.appendChild(label);
+}
+root.appendChild(stylePicker);
 for (const rolePicker of rolePickers) {
     root.appendChild(rolePicker);
 }
